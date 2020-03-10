6e35a2e8aceecf284f1b4be1f2f5d0bdd34c3040
brian <bchou515@gmail.com>
2020-03-10 17:52:33 -0400

    missing message text field

--- web/models.py
+++ web/models.py
@@ -17,18 +17,24 @@ class Chatroom(db.Model):
     name = db.Column(db.String(50))
     memberships = db.relationship("Membership", lazy=True)
 
+    def __repr__(self):
+        return f"Chatroom('{self.id}','{self.name}')"
+
 class Membership(db.Model):
     __table_args__ = (
         db.PrimaryKeyConstraint('chat_id', 'member_id'),
     )
     chat_id = db.Column(db.Integer, db.ForeignKey('chatroom.id'))
     member_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, default=datetime.now)
+    text = db.Column(db.String(250))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     room_id = db.Column(db.Integer, db.ForeignKey('chatroom.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Message('{self.id}', '{self.date}', '{self.message}')"
 
 
