6697c93da5b0a6f8e9e491aecd57061dbc6d6a78
brian <bchou515@gmail.com>
2020-03-10 23:57:34 -0400

    cleaner chat messaging

--- web/events.py
+++ web/events.py
@@ -7,7 +7,15 @@ def messageReceived(methods=['GET','POST']):
     print('message was recieved')
 
 @socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print(json)
-    json['user'] = current_user.name
-    socketio.emit('my response', json, callback=messageReceived)
+def user_online(json, methods=['GET', 'POST']):
+    # new_message = Message(author_id=current_user.id, text=json['message'], room_id=json['chat_id'])
+    # db.session.add(new_message)
+    # db.session.commit()
+    if 'data' in json:
+        print("user online")
+        print(json['data'])
+        socketio.emit('my response', {'data':json['data']}, callback=messageReceived)
+    else:
+        print("typing happened")
+        json['user'] = current_user.name
+        socketio.emit('my response', json, callback=messageReceived)
