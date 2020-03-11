6c9c62fed5b4d21631a5589d7dd3a49816280c05
brian <bchou515@gmail.com>
2020-03-11 13:16:25 -0400

    commit to database

--- web/events.py
+++ web/events.py
@@ -8,14 +8,13 @@ def messageReceived(methods=['GET','POST']):
 
 @socketio.on('my event')
 def user_online(json, methods=['GET', 'POST']):
-    # new_message = Message(author_id=current_user.id, text=json['message'], room_id=json['chat_id'])
-    # db.session.add(new_message)
-    # db.session.commit()
     if 'data' in json:
-        print("user online")
         print(json['data'])
         socketio.emit('my response', {'data':json['data']}, callback=messageReceived)
     else:
         print("typing happened")
         json['user'] = current_user.name
+        new_message = Message(author_id=current_user.id, text=json['message'], room_id=json['chat_id'])
+        db.session.add(new_message)
+        db.session.commit()
         socketio.emit('my response', json, callback=messageReceived)
